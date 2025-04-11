3427bd79c1385a07fcdd66cafe88a5d758d32c65
Sivasubramani V <sivasubramani17v@gmail.com>
2025-04-11 12:03:28 +0530

    Time: 1 ms (16.69%), Space: 41 MB (20.62%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,23 +1,23 @@
 class Solution {
-    // public int hammingWeight(int n) {
-    //     int ones = 0;
-
-    //     while( n!= 0){
-    //         n= n&n-1;
-    //         ones++;
-    //     }
-    //     return ones;
-    // }
-
     public int hammingWeight(int n) {
-    String binary = Integer.toBinaryString(n);
-    int ones = 0;
-    for (int i = 0; i < binary.length(); i++) {
-        if (binary.charAt(i) == '1') {
+        int ones = 0;
+
+        while( n!= 0){
+            n= n&n-1;
             ones++;
         }
+        return ones;
     }
-    return ones;
-}
+
+//     public int hammingWeight(int n) {
+//     String binary = Integer.toBinaryString(n);
+//     int ones = 0;
+//     for (int i = 0; i < binary.length(); i++) {
+//         if (binary.charAt(i) == '1') {
+//             ones++;
+//         }
+//     }
+//     return ones;
+// }
 
 }
