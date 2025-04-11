e6cd8177eb42d9d384b65b9bd4d326f5d02ad99c
Sivasubramani V <sivasubramani17v@gmail.com>
2025-04-11 11:42:38 +0530

    Time: 38 ms (27.85%), Space: 45.1 MB (8.51%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,22 +1,56 @@
+// class Solution {
+//     public int countSymmetricIntegers(int low, int high) {
+//         int count = 0;
+//         for (int i = low; i <= high; i++) {
+//             if (isSymmetric(i)) count++;
+//         }
+//         return count;
+//     }
+
+//     public static boolean isSymmetric(int x) {
+//         String s = Integer.toString(x);
+//         int n = s.length();
+//         if (n % 2 != 0) return false;
+
+//         int sum1 = 0, sum2 = 0;
+//         for (int i = 0; i < n / 2; i++) {
+//             sum1 += s.charAt(i) - '0';
+//             sum2 += s.charAt(n - 1 - i) - '0';
+//         }
+//         return sum1 == sum2;
+//     }
+// }
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
         int count = 0;
         for (int i = low; i <= high; i++) {
-            if (isSymmetric(i)) count++;
+            count += isSymmetric(i);
         }
         return count;
     }
 
-    public static boolean isSymmetric(int x) {
-        String s = Integer.toString(x);
-        int n = s.length();
-        if (n % 2 != 0) return false;
-        
-        int sum1 = 0, sum2 = 0;
-        for (int i = 0; i < n / 2; i++) {
-            sum1 += s.charAt(i) - '0';
-            sum2 += s.charAt(n - 1 - i) - '0';
+    public int isSymmetric(int num) {
+        int len = (num == 0) ? 1 : (int) Math.log10(num) + 1;
+
+        if (len % 2 != 0) return 0;
+
+        int[] digits = new int[len];
+        int temp = num;
+
+        // Extract digits from right to left
+        for (int i = len - 1; i >= 0; i--) {
+            digits[i] = temp % 10;
+            temp /= 10;
         }
-        return sum1 == sum2;
+
+        int half = len / 2;
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < half; i++) {
+            sum1 += digits[i];
+            sum2 += digits[i + half];
+        }
+
+        return sum1 == sum2 ? 1 : 0;
     }
 }
