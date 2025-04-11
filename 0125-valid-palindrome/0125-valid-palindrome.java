8ee1f3e5dda949e9ea5bbb94a8400fd84bceab9d
Sivasubramani V <sivasubramani17v@gmail.com>
2025-04-11 10:01:01 +0530

    Time: 4 ms (57.53%), Space: 43.4 MB (59.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-StringBuilder S = new StringBuilder(); 
-for(int i = 0; i < s.length(); i++) {
-    char c = s.charAt(i);  
-    if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'|| s.charAt(i)>='0' && s.charAt(i) <= '9')) {
-        S.append(Character.toLowerCase(c)); 
-    }
-}
-//   String n = S.toString();
-//   if( s.length()!= n.length()){
-//     return false;
-//   }
-StringBuilder SS = new StringBuilder(S).reverse();
-return SS.toString().equals(S.toString());
+        StringBuilder S = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z' || s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
+                S.append(Character.toLowerCase(c));
+            }
+        }
+              
+        StringBuilder SS = new StringBuilder(S);
+        SS = SS.reverse();
+        return SS.toString().equals(S.toString());
     }
 }
