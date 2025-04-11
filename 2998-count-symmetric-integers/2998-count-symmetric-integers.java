352cb7972ad319954020581346bfac7e1b807ab4
Sivasubramani V <sivasubramani17v@gmail.com>
2025-04-11 11:07:46 +0530

    Time: 28 ms (45.89%), Space: 44.6 MB (52.09%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,27 +1,22 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-
         int count = 0;
-        if( low == high && low >=0 && high <=10) return 0;
         for (int i = low; i <= high; i++) {
-            if (isSymmetric(i))
-                count++;
+            if (isSymmetric(i)) count++;
         }
         return count;
     }
 
-    public static boolean isSymmetric(int x){
-            String x1 = Integer.toString(x);
-            if(x1.length()%2 == 1) return false;
-
-            //String firstHalf = x1.substring(0,x1.length()/2);
-            //String secondHalf = x1.substring(x1.length()/2;
-            int sum1 =0, sum2=0;
-            int j= (x1.length())-1;
-            for( int i=0;i<x1.length()/2;i++,j--){
-                sum1+= x1.charAt(i)-'0';
-                sum2+= x1.charAt(j)-'0';
-            }
-            return sum1 == sum2;
+    public static boolean isSymmetric(int x) {
+        String s = Integer.toString(x);
+        int n = s.length();
+        if (n % 2 != 0) return false;
+        
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < n / 2; i++) {
+            sum1 += s.charAt(i) - '0';
+            sum2 += s.charAt(n - 1 - i) - '0';
         }
+        return sum1 == sum2;
+    }
 }
