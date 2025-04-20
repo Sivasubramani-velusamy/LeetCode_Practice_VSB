aae86b5c1fa6e2a9d9fbdcfdef81ea2bdc57a28a
Sivasubramani V <sivasubramani17v@gmail.com>
2025-04-20 14:25:41 +0530

    Time: 2 ms (88.81%), Space: 41.6 MB (48%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -3,8 +3,8 @@ class Solution {
         int[] d = Arrays.copyOf(heights, heights.length);
         Arrays.sort(d);
         int count =0;
-        System.out.println(Arrays.toString(heights));
-        System.out.println(Arrays.toString(d));
+        // System.out.println(Arrays.toString(heights));
+        // System.out.println(Arrays.toString(d));
         for (int i = 0; i < heights.length; i++) {
             if (heights[i] != d[i])
                 count++;
